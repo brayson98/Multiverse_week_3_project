2307647fa88ba23a180425e5f0b73aaded916e32
brayson98 <104509853+brayson98@users.noreply.github.com>
2024-10-18 10:12:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 document.getElementById('toggle-mode').addEventListener('click', () => {
     document.body.classList.toggle('dark-mode');
 });
+
+document.getElementById('current-year').textContent = new Date().getFullYear();
